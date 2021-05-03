c86121eb58a23ab4e524453ed43586db08ab5e6e
SAKTY'S <Sakti Anugrah>
2021-05-04 01:04:59 +0700

    Header, Footer and Login Page Done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+  <div class="card-header">
+    <h3>FOOTER</h3>
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>The NewTrade MarketPlace about Anything, especially Indonesia Resourcess Plants </p>
+      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+    </blockquote>
+  </div>
+</div>
